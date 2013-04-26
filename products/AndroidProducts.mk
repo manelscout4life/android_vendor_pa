02f087a6e9fdf5d294254765f1eafd968fa7704f
Joao <simoesj90@gmail.com>
2013-04-26 13:53:50 -0500

    Add t0lte remove nav bar
    Change-Id: Ic2c2641b94b64f324643957315d5946ccd51074d

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -31,6 +31,9 @@ endif
 ifeq (pa_toro,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_toro.mk
 endif
+ifeq (pa_t0lte,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_t0lte.mk
+endif
 ifeq (pa_toroplus,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_toroplus.mk
 endif
